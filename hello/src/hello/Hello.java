f37d452a64eadea8972aa821affcff9cca0278a5
dooroom <dooroom@DESKTOP-34NVGL5>
2021-10-10 22:37:49 +0900

    a

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("hello world");
-		System.out.println("hello world");
+		System.out.println("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");
 	}
 
 }
